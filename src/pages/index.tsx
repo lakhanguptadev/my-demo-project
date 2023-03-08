ea3999cdad9cfc52f6b3a328fcdad86f5d626bb6
lakhanguptadev <127130023+lakhanguptadev@users.noreply.github.com>
2023-03-08 13:49:29 +0000

    my first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+
+
+
+export default function Home() {
+  return (
+    <>
+      <h1>Hello world</h1>
+      <h4>This is utkarsh website ❤️</h4>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad facilis, sapiente neque esse eum suscipit minus id amet similique incidunt error assumenda culpa accusantium illo labore dicta? Architecto, dignissimos quasi!</p>
+    </>
+  )
+}
